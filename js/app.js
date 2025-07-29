8ced5d70b9e27db638033cc7d04aa5bddd77dd94
paulo-peyerl <paulo.peyerl1@gmail.com>
2025-07-29 20:31:38 -0300

    adiciona variável para referência da lista de amigos na função adicionar

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 let arrayDoNomeDosAmigos = [];
 function adicionar(){
     let nomesDosAmigos = document.getElementById('nome-amigo').value;
+    let listaDosAmigos = document.getElementById('lista-amigos');
     if(nomesDosAmigos === '' ){
         alert('O campo não pode estar vazio.');
         return;
     }
+    
     arrayDoNomeDosAmigos.push(nomesDosAmigos);
+    listaDosAmigos.innerHTML = arrayDoNomeDosAmigos.join(', ');
     document.getElementById('nome-amigo').value = '';
 }
